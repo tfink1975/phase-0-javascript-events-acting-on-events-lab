33c3291a4603f5e23ecde86a918cb7521823b346
Tim <tim.fink519@gmail.com>
2022-06-11 16:13:57 -0400

    commit tree aae886de930d1aa90529bb593367076fcf0b24b4

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Your code here
+const dodger = document.getElementById("dodger");
+dodger.style.backgroundColor = "#FF69B4";
+dodger.style.bottom = "0px";
+dodger.style.left = "0px";
+
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > -1) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  }); 
+  document.addEventListener("keydown", function (event) {
+    if (event.key === "ArrowLeft") {
+      const leftNumbers = dodger.style.left.replace("px", "");
+      const left = parseInt(leftNumbers, 10);
+  
+      dodger.style.left = `${left - 1}px`;
+    }
+  });
+  function moveDodgerRight() {
+      const newPosition = dodger.style.left.replace("px", "");
+      const right = parseInt(newPosition, 10);
+
+      if (newPosition < 500) {
+          dodger.style.left = `${right + 1}px`;
+      }
+  }
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowRight") {
+      moveDodgerRight();
+    }
+  });
